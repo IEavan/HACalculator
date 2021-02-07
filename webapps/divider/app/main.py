118a42b107b82096cace2ed5e9ca4d5836e50b3b
Eavan Pattie <eavan@lyse.net>
2021-02-07 15:42:02 +0100

    handle non-integers and expressions with more than one operator

--- webapps/divider/app/main.py
+++ webapps/divider/app/main.py
@@ -11,4 +11,4 @@ def execute():
     return jsonify({"error":"request is not json"})
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
+    app.run(host="0.0.0.0", port=8084)
--- webapps/parser/app/main.py
+++ webapps/parser/app/main.py
@@ -1,12 +1,11 @@
 import requests
 import re
-import json
 from flask import Flask, jsonify, request
 app = Flask(__name__)
 
 MULTIPLICATIVE = re.compile(r"(?P<left>.*?)(?P<operator>\*|\/)(?P<right>.*)")
 ADDIDITIVE     = re.compile(r"(?P<left>.*?)(?P<operator>\+|\-)(?P<right>.*)")
-NUM            = re.compile(r"\s*(\d+)\s*")
+NUM            = re.compile(r"\s*(\d+(?:\.\d+)?)\s*")
 
 
 URLS = {
@@ -22,31 +21,39 @@ def execute():
         data = request.get_json()
         expression = data["expression"]
 
-        match = ADDIDITIVE.fullmatch(expression)
-        if match is not None:
-            left = int(match.group("left"))
-            op = match.group("operator").strip()
-            right = int(match.group("right"))
-            
-
-        
-        match = MULTIPLICATIVE.fullmatch(expression)
-        if match is not None:
-            left = int(match.group("left"))
-            op = match.group("operator").strip()
-            right = int(match.group("right"))
-
-
-        print(f"left = {left}, op = {op}, right = {right}")
-        request_data = {
-                "terms": [left, right]
-        }
-        response = requests.post(URLS[op], json=request_data)
-        print(response.status_code)
-
-        return str(response.json())
+        return str(evaluate(expression)) + "\n"
 
     return jsonify({"error":"Nothing to do"})
+
+
+def evaluate(expression):
+    match = ADDIDITIVE.fullmatch(expression)
+    if match is None:
+        match = MULTIPLICATIVE.fullmatch(expression)
+    if match is None:
+        print(f"Expression '{expression}' not understood")
+        return 0
+
+    left = match.group("left").strip()
+    op = match.group("operator").strip()
+    right = match.group("right").strip()
+
+    if NUM.fullmatch(left):
+        left = float(left)
+    else:
+        left = evaluate(left)
+
+
+    if NUM.fullmatch(right):
+        right = float(right)
+    else:
+        right = evaluate(right)
+
+    request_data = {
+            "terms": [left, right]
+    }
+    response = requests.post(URLS[op], json=request_data)
+    return response.json()
 
 
 if __name__ == "__main__":
